ad32e3e7b0963068531aaacdbd82ef4420fbc78d
29449 <2944972991@qq.com>
2021-06-14 22:55:51 +0800

    字符串切片的实际运用

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,38 @@
 fn main() {
     let mut s = String::from("aaaaa");
     let world = first_world(&s);
-    println!("{}",world);
+    //清空字符串
+    s.clear();
+    println!("{}", world);
+
+    //===================字符串切片===================
+    let a = String::from("Hello world");
+    //&a:对某字符串的引用
+    //[0..5]:引用该字符串的一部分，起始为0可以不写0[..5],结束为字符最后长度也可不写[6..]
+    let b = &a[0..5];
+    let c = &a[6..11];
+    //切片为整个字符串时,前后都可以省略不写,或者0..a.len();
+    let e = &a[..];
+
+    //===================字符串切片重写列子===================
+    let mut f = String::from("aa aaa");
+    let worldTwo = first_worldTwo(&f);
+    //此时清空字符串方法将报错，因为该变量已经成为不可变的
+    f.clear();
+    println!("{}", worldTwo);
+
+    //===================字符串切片作为参数传递===================
+    let mut g = String::from("aa aaa");
+    let worldThree = first_worldThree(&g[..]);//传入该字符串的整个字符串切片
+    //此时清空字符串方法将报错，因为该变量已经成为不可变的
+    g.clear();
+    println!("{}", worldThree);
+
+    let mut  h= String::from("aa aaa");
+    let worldFour = first_worldThree(&h);//传入该字符串的整个字符串
+    //此时清空字符串方法将报错，因为该变量已经成为不可变的
+    h.clear();
+    println!("{}", worldFour);
 }
 
 /*
@@ -21,4 +52,42 @@ fn first_world(s: &String) -> usize {
         }
     }
     s.len()
+}
+
+//===================字符串切片重写列子===================
+//&str:表示返回字符串切片
+fn first_worldTwo(s: &String) -> &str {
+    //as_bytes():返回字节数组
+    let bytes = s.as_bytes();
+    //iter():生成一个迭代器
+    //enumerate():将结果进行包装，每个包装作为元组的一部分进行返回
+    //(i,&item):生成模式匹配，在里面生成两个变量对元组进行解构
+    for (i, &item) in bytes.iter().enumerate() {
+        //判断是否为空格的字面值:b' '
+        if item == b' ' {
+            //找到空格时,截取字符串开始到第i位的切片，并返回
+            return &s[..i];
+        }
+    }
+    //如果没有则返回整个字符串切片
+    &s[..]
+}
+
+//===================字符串切片作为参数传递===================
+//&str:表示传入和返回字符串切片
+fn first_worldThree(s: &str) -> &str {
+    //as_bytes():返回字节数组
+    let bytes = s.as_bytes();
+    //iter():生成一个迭代器
+    //enumerate():将结果进行包装，每个包装作为元组的一部分进行返回
+    //(i,&item):生成模式匹配，在里面生成两个变量对元组进行解构
+    for (i, &item) in bytes.iter().enumerate() {
+        //判断是否为空格的字面值:b' '
+        if item == b' ' {
+            //找到空格时,截取字符串开始到第i位的切片，并返回
+            return &s[..i];
+        }
+    }
+    //如果没有则返回整个字符串切片
+    &s[..]
 }
